d38fa0dbdf1ff6befd3cfbec81972bb1e337a0ab
小傅哥 <184172133@qq.com>
2021-09-06 07:06:46 +0800

    小傅哥，docs：规范代码代码注释

--- lottery-domain/src/main/java/cn/itedus/lottery/domain/strategy/model/vo/AwardRateInfo.java
+++ lottery-domain/src/main/java/cn/itedus/lottery/domain/strategy/model/vo/AwardRateInfo.java
@@ -3,18 +3,22 @@ package cn.itedus.lottery.domain.strategy.model.vo;
 import java.math.BigDecimal;
 
 /**
- * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- * 公众号：bugstack虫洞栈
- * Create by 小傅哥(fustack)
- *
- * 奖品概率信息，奖品编号、库存、概率
+ * @description: 奖品概率信息，奖品编号、库存、概率
+ * @author: 小傅哥，微信：fustack
+ * @date: 2021/9/4
+ * @github: https://github.com/fuzhengwei
+ * @Copyright: 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
  */
 public class AwardRateInfo {
 
-    // 奖品ID
+    /**
+     * 奖品ID
+     */
     private String awardId;
 
-    // 中奖概率
+    /**
+     * 中奖概率
+     */
     private BigDecimal awardRate;
 
     public AwardRateInfo() {
