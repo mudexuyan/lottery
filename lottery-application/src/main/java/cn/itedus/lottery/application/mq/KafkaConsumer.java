cd0ce86cd1c7d52ad34ea90960f0a70ed4867dfd
小傅哥 <184172133@qq.com>
2021-10-23 14:12:14 +0800

    小傅哥，feat：第15节：第15节：搭建MQ消息组件Kafka服务环境

---
+++ lottery-application/src/main/java/cn/itedus/lottery/application/mq/KafkaConsumer.java
@@ -0,0 +1,36 @@
+package cn.itedus.lottery.application.mq;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+/**
+ * @description: 消息消费者
+ * @author: 小傅哥，微信：fustack
+ * @date: 2021/10/23
+ * @github: https://github.com/fuzhengwei
+ * @Copyright: 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ */
+@Component
+public class KafkaConsumer {
+
+    private Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = KafkaProducer.TOPIC_TEST, groupId = KafkaProducer.TOPIC_GROUP)
+    public void topicTest(ConsumerRecord<?, ?> record, Acknowledgment ack, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        Optional<?> message = Optional.ofNullable(record.value());
+        if (message.isPresent()) {
+            Object msg = message.get();
+            logger.info("topic_test 消费了： Topic:" + topic + ",Message:" + msg);
+            ack.acknowledge();
+        }
+    }
+
+}
