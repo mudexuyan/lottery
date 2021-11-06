82539608466ffc81e9fa252ace25a9f8675dfd42
fuzhengwei <184172133@qq.com>
2021-11-06 15:50:19 +0800

    小傅哥,feat：第17节：引入xxl-job处理活动状态扫描

--- lottery-infrastructure/src/main/java/cn/itedus/lottery/infrastructure/dao/IActivityDao.java
+++ lottery-infrastructure/src/main/java/cn/itedus/lottery/infrastructure/dao/IActivityDao.java
@@ -5,6 +5,7 @@ import cn.itedus.lottery.domain.activity.model.vo.AlterStateVO;
 import cn.itedus.lottery.infrastructure.po.Activity;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -35,16 +36,25 @@ public interface IActivityDao {
     /**
      * 变更活动状态
      *
-     * @param alterStateVO  [activityId、beforeState、afterState]
+     * @param alterStateVO [activityId、beforeState、afterState]
      * @return 更新数量
      */
     int alterState(AlterStateVO alterStateVO);
 
     /**
      * 扣减活动库存
+     *
      * @param activityId 活动ID
      * @return 更新数量
      */
     int subtractionActivityStock(Long activityId);
+
+    /**
+     * 扫描待处理的活动列表，状态为：通过、活动中
+     *
+     * @param id ID
+     * @return 待处理的活动集合
+     */
+    List<Activity> scanToDoActivityList(Long id);
 
 }
